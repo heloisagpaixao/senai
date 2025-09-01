4934d4df4fd18a476eade52cca971f50a5df1bf0
heloisagpaixao <heloisa.paixao@aluno.senai.br>
2025-09-01 15:59:02 -0300

    edições e adição da lista de exercício
    programação orientada a objeto

---
+++ SENAI_2etapa/backEnd/POO/exemplos_aula1.js
@@ -0,0 +1,72 @@
+// const pessoa = {
+//     nome: `João`,
+//     idade: 30,
+//     falar: function() {
+//         console.log(`Olá! Meu nome é ${this.nome}!`)
+// }};
+// pessoa.falar();
+
+// const pessoa2 = {
+//     nome: `Maria`,
+//     idade: 26,
+//     falar: function() {
+//         console.log(`Olá! Minha idade é ${this.idade}!`)
+// }};
+// pessoa2.falar();
+
+// const pessoa3 = {
+//     nome: `Renato`,
+//     idade: 18,
+//     falar: function() {
+//         console.log(`Olá! Meu nome é ${this.nome}!`)
+// }};
+// pessoa3.falar();
+
+// const pessoa4 = {
+//     nome: `Ana`,
+//     idade: 42,
+//     falar: function() {
+//         console.log(`Olá! Minha idade é ${this.idade}!`)
+// }};
+// pessoa4.falar();
+
+
+// class Pessoa {
+//     constructor(nome, idade){
+//     this.nome = nome;
+//     this.idade = idade;
+// }
+
+
+// saudacao () {
+//     return console.log (`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
+// }}
+
+// // Criando um objeto a partir da classe Pessoa
+// const pessoa1 = new Pessoa(`João`, 25);
+// // Chamando o método do objeto
+// pessoa1.saudacao()
+
+
+class Conta {
+    constructor(titular, saldo){
+    this.titular = titular;
+    this.saldo = saldo;
+}
+
+depositar (valor) {
+    this.saldo += valor
+    console.log(`Depósito realizado. Saldo atual de R$ ${this.saldo}`)
+}
+sacar (valor) {
+    if(valor <= this.saldo){
+        this.saldo -= valor
+        console.log(`Saque realizado. Saldo atual de R$ ${this.saldo}.`)
+    } else {
+        console.log(`Saldo insuficiente!`)
+    }
+}}
+
+let conta1 =  new Conta(`Celso`, 0)
+conta1.depositar(2000)
+conta1.sacar(510)
---
+++ SENAI_2etapa/backEnd/POO/heloisaPaixao_lista-POO.js
@@ -0,0 +1,82 @@
+// // Exercício 1
+// class Produto {
+//     constructor(nome, preco, quantidade){
+//     this.nome = nome;
+//     this.preco = preco;
+//     this.quantidade = quantidade; 
+// }
+
+// calcular (){
+//     return console.log (`O valor total do estoque de ${this.nome} é de R$ ${this.preco * this.quantidade}.`)
+// }}
+
+// let produto1 = new Produto(`Banana`, 20, 2)
+// produto1.calcular()
+
+
+// // Exercício 2
+// class Filme {
+//     constructor(titulo, diretor, ano){
+//     this.titulo = titulo;
+//     this.diretor = diretor;
+//     this.ano = ano; 
+// }
+
+// exibir (){
+//     return console.log (`O filme ${this.titulo} foi lançado em ${this.ano} e foi dirigido por ${this.diretor}.`)
+// }}
+
+// let filme1 = new Filme(`Lorax`, `Chris Renaud`, 2012)
+// filme1.exibir()
+
+
+// // Exercício 3
+// class Aluno {
+//     constructor(nome, nota1, nota2){
+//     this.nome = nome;
+//     this.nota1 = nota1;
+//     this.nota2 = nota2;
+// }
+
+// media (){
+//     return (this.nota1 + this.nota2)/2
+// }
+
+// exibir (){
+//     if(this.media() > 0 && this.media() < 7){
+//         console.log(`Sua média foi igual a ${this.media()}, você não foi aprovado!`)
+//     } else if (this.media() >= 7 && this.media() <= 10){
+//         console.log(`Sua média foi igual a ${this.media()}, você foi aprovado!`)
+//     } else {
+//         console.log(`Olá ${this.nome}! Algum valor está inválido!`)
+// }}}
+
+// let aluno1 = new Aluno(`Letícia`, 4, 2)
+// aluno1.media()
+// aluno1.exibir()
+
+
+// Exercício 4
+class Retangulo {
+    constructor(base, altura){
+    this.base = base;
+    this.altura = altura;
+}
+
+area (){
+    return (this.base * this.altura)
+}
+
+perimetro(){
+    return (this.base * 2) + (this.altura * 2)
+}
+
+exibir(){
+    return console.log(`O retângulo tem ${this.area()} cm² de área e ${this.perimetro()} cm de perímetro!`)
+}
+}
+
+let retangulo1 = new Retangulo(4, 5)
+retangulo1.area()
+retangulo1.perimetro()
+retangulo1.exibir()
