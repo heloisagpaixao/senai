d0aa3c17455ae745eaa188a028ba7c972bdc17de
heloisagpaixao <heloisa.paixao@aluno.senai.br>
2025-09-24 16:01:41 -0300

    exercícios - 24/09
    Matéria de Banco de Dados

--- SENAI_2etapa/bancoDeDados/SQL_files/aula1SQL.sql
+++ SENAI_2etapa/bancoDeDados/SQL_files/aula1SQL.sql
@@ -10,8 +10,9 @@ create table tbl_livros (
 );
 
 create index idx_titulo on tbl_livros (titulo);
-
 INSERT INTO tbl_livros (id_livro, titulo, autor, ano_publicacao, preco) VALUES
 (1, 'Dom Casmurro', 'Machado de Assis', 1899, 39.90),
 (2, 'O Alquimista', 'Paulo Coelho', 1988, 29.50),
 (3, 'A Hora da Estrela', 'Clarice Lispector', 1977, 34.00);
+
+select * from tbl_livro;
--- SENAI_2etapa/bancoDeDados/SQL_files/heloisaPaixao_cenarios-exercicio/heloisaGabriellyPaixao_cenarios-mySQL.sql
+++ SENAI_2etapa/bancoDeDados/SQL_files/heloisaPaixao_cenarios-exercicio/heloisaGabriellyPaixao_cenarios-mySQL.sql
@@ -2,15 +2,21 @@ create user 'secretaria'@'localhost' IDENTIFIED BY 'Escola@Sec1';
 create user 'professor'@'localhost' IDENTIFIED BY 'Escola@Prof2';
 create database db_escola_b;
 use db_escola_b;
+
 create table tbl_alunos (
 	id_aluno INT,
 	nome VARCHAR(100),
-	data_nascimento DATE
+	data_nascimento DATE,
+    media_final DECIMAL(4,2)
 );
+
 grant all privileges on db_escola_b.tbl_alunos to 'secretaria'@'localhost';
 grant select on db_escola_b.tbl_alunos to 'professor'@'localhost';
 grant update, select on db_escola_b.tbl_alunos to 'professor'@'localhost';
 revoke all privileges on db_escola_b.tbl_alunos from 'secretaria'@'localhost';
+
+drop user 'secretaria'@'localhost';
+drop user 'professor'@'localhost';
 
 use db_biblioteca_b;
 drop table tbl_livros;
