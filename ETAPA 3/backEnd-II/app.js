c8a9aff4748760b9d4d5aee42ce91517a0405887
heloísa <heloisa.paixao@aluno.senai.br>
2026-02-05 10:31:03 -0300

    atualizações (05/02/2026)

--- ETAPA 3/backEnd-II/app.js
+++ ETAPA 3/backEnd-II/app.js
@@ -1,5 +1,29 @@
 import express from "express"
+import { getEnvironmentData } from "node:worker_threads"
 
 const app = express()
+
+app.use (express.json())
+
+const musicas = [
+    {
+        id: 12,
+        titulo: "Chicago",
+        artista: "Michael Jackson",
+        genero: "pop",
+        ano_publicacao: 1999
+    }
+]
+
+function buscarMusica(id){
+    return musicas.findIndex(m => {
+        return m.id === Number(id)
+    })
+}
+
+app.get("/", (req, res) => {
+    res.status(200).send("CRUD Músicas")
+})
+
 export default app
 
